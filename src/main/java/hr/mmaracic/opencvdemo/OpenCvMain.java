c67d07630bb6b616e4bb20d4254266c28e74e32e
Marijo Maračić <marijo.maracic@gmail.com>
2017-10-31 08:21:54 +0100

    Initial commit

--- src/main/java/hr/mmaracic/opencvdemo/OpenCvMain.java
+++ src/main/java/hr/mmaracic/opencvdemo/OpenCvMain.java
@@ -5,7 +5,11 @@
  */
 package hr.mmaracic.opencvdemo;
 
+import java.awt.Image;
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import static org.opencv.imgcodecs.Imgcodecs.CV_LOAD_IMAGE_COLOR;
 
 /**
  *
@@ -16,6 +20,11 @@ public class OpenCvMain{
     public static void main(String[] args) {
         // load the native OpenCV library
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        
+        String fileName = ClassLoader.getSystemClassLoader().getResource("lena.png").getFile();
+        Mat img = Imgcodecs.imread(fileName, CV_LOAD_IMAGE_COLOR);
+        Image javaImg = Util.toBufferedImage(img);
+        Util.displayImage(javaImg);
     }
 
 }
---
+++ src/main/java/hr/mmaracic/opencvdemo/Util.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hr.mmaracic.opencvdemo;
+
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import org.opencv.core.Mat;
+
+/**
+ *
+ * @author Marijo
+ */
+public class Util {
+
+    //http://answers.opencv.org/question/10344/opencv-java-load-image-to-gui/
+    public static Image toBufferedImage(Mat m) {
+        int type = BufferedImage.TYPE_BYTE_GRAY;
+        if (m.channels() > 1) {
+            type = BufferedImage.TYPE_3BYTE_BGR;
+        }
+        int bufferSize = m.channels() * m.cols() * m.rows();
+        byte[] b = new byte[bufferSize];
+        m.get(0, 0, b); // get all the pixels
+        BufferedImage image = new BufferedImage(m.cols(), m.rows(), type);
+        final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        System.arraycopy(b, 0, targetPixels, 0, b.length);
+        return image;
+    }
+
+    public static void displayImage(Image img) {
+        ImageIcon icon = new ImageIcon(img);
+        JFrame frame = new JFrame();
+        frame.setLayout(new FlowLayout());
+        frame.setSize(img.getWidth(null) + 50, img.getHeight(null) + 50);
+        JLabel lbl = new JLabel();
+        lbl.setIcon(icon);
+        frame.add(lbl);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
